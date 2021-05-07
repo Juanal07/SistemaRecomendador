0f1f0198f4b144b8abd50f67de2a6bf28e33b8e4
Alvaro Gamero <gamerojtm@gmail.com>
2021-05-07 21:56:16 +0200

    mejoras

--- funciones.py
+++ funciones.py
@@ -1,3 +1,10 @@
-def prueba1():
-    a = "hello wordl"
-    return a
+from PyQt5.QtWidgets import QComboBox, QTableWidget, QTableWidgetItem
+
+def insertarComboBox(combobox, usuarios):
+    combobox.addItems(usuarios)
+
+def insertarTabla(tabla, datos, filas):
+    tabla.setRowCount(filas)
+    for fila in range(0, len(datos)):
+        for columna in range(0, len(datos[0])):
+            tabla.setItem(fila, columna, QTableWidgetItem(datos[fila][columna]))
