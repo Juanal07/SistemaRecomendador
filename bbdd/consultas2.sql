07b38ba6c3ce4da79e25190a55119c888b732649
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-05-12 00:32:23 +0200

    actualizo sqls de consultas

--- bbdd/consultas2.sql
+++ bbdd/consultas2.sql
@@ -1,1 +1,1 @@
- SELECT userId,rating, movieId FROM rating WHERE movieId=1 AND userId IN (SELECT userId FROM rating WHERE movieId=3);
+SELECT userId,rating, movieId FROM rating WHERE movieId=1 AND userId IN (SELECT userId FROM rating WHERE movieId=3);
